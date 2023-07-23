adc2c952378c493f324406b504350f2daa742089
PUSHRSP <pushrsp@gmail.com>
2023-07-23 13:58:29 +0900

    메일 테스트 with mock

---
+++ src/test/java/sample/firsttest/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,43 @@
+package sample.firsttest.spring.api.service.mail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sample.firsttest.spring.client.mail.MailSendClient;
+import sample.firsttest.spring.domain.history.mail.MailSendHistory;
+import sample.firsttest.spring.domain.history.mail.MailSendHistoryRepository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    public void sendMail() throws Exception {
+        //given
+        given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
+        //when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+}
